57b6193f6f6fa1e32f89ebb62c98b3b58559725f
blue_sprite <hasheminejadfatemeh3@gmail.com>
2018-09-12 02:12:23 -0700

    chi begam

--- src/new/src/talis.cpp
+++ src/new/src/talis.cpp
@@ -6,19 +6,23 @@
 
 int x,y;
 int a,b;
+geometry_msgs::Twist message;
 
-void chatterCallback(const turtlesim::Pose::ConstPtr& msg)
+void chatterCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
- x=msg->x;
- y=msg->y;
+ x= msg->linear.x;
+ y= msg->linear.y;
+message.angular.x =msg->angular.x;
+message.angular.y =msg->angular.y;
+
   ROS_INFO("I heard: [%f],[%f]", x,y);
 }
 
-void chatterCallback1(const turtlesim::Pose::ConstPtr& msg)
+void chatterCallback1(const geometry_msgs::Twist::ConstPtr& msg)
 {
- a=msg->x;
- b=msg->y;
-  ROS_INFO("I heard: [%f],[%f]",a,b);
+// a=msg.linear.x;
+// b=msg.linear.y;
+  //ROS_INFO("I heard: [%f],[%f]",a,b);
 }
 
 int main(int argc, char **argv)
@@ -28,8 +32,8 @@ int main(int argc, char **argv)
 
   
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("turtle1/pose", 1000, &chatterCallback);
-  ros::Subscriber sub1 = n.subscribe("akbar/pose", 1000, &chatterCallback1);
+  ros::Subscriber sub = n.subscribe("turtle1/cmd_vel", 1000, &chatterCallback);
+ // ros::Subscriber sub1 = n.subscribe("akbar/cmd_vel", 1000, &chatterCallback1);
 
   
   ros::Publisher chatter_pub = n.advertise<geometry_msgs::Twist>("akbar/cmd_vel", 1000);
@@ -41,11 +45,13 @@ int main(int argc, char **argv)
   while (ros::ok())
   {
     geometry_msgs::Twist msg;
-
-    msg.linear.x=x-a;
-    msg.linear.y=y-b;
-
-    ROS_INFO("%f  %f",x-a,y-b );
+    
+    msg.linear.x=message.linear.x;
+    msg.linear.y=message.linear.y;
+    
+    msg.angular.x=message.angular.x;
+    msg.angular.y=message.angular.y;
+    ROS_INFO("lab lab %f  %f",msg.linear.x,msg.linear.y );
 
     
     chatter_pub.publish(msg);
